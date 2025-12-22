812e5596b461a57a4ed38df030e0fe12bb7ef600
gokhoso <klasalperen334@gmail.com>
2025-12-22 20:58:40 +0300

    Added new method.

---
+++ src/main/java/net/javac/utils/TextUtils.java
@@ -0,0 +1,23 @@
+package net.javac.utils;
+
+
+public class TextUtils {
+    public static String createChannelMention(String channelId) {
+        return String.format("<#%s>", channelId);
+    }
+    public static String createUserMention(String userId) {
+        return String.format("<@%s>", userId);
+    }
+    public static String setVariable(String text, String guildId, String memberId, String memberName) {
+        if (text.contains("{count}")) {
+            return text.replace("{count}", Integer.toString(GuildUtils.getCount(guildId)));
+        }
+        if (text.contains("{member.name}")) {
+            return text.replace("{member.name}", memberName);
+        }
+        if (text.contains("{member.mention}")) {
+            return text.replace("{member.mention}", TextUtils.createUserMention(memberId));
+        }
+        return text;
+    }
+}
