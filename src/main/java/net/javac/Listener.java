f9b18802901ae436a2ec581506ca3191dff0aa8e
gokhoso <klasalperen334@gmail.com>
2025-12-22 20:57:38 +0300

    Added WelcomeMessageSender.

--- src/main/java/net/javac/Listener.java
+++ src/main/java/net/javac/Listener.java
@@ -1,6 +1,7 @@
 package net.javac;
 
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
@@ -8,11 +9,13 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.javac.buffer.impl.GuildMessageBuffer;
 import net.javac.log.LogManager;
 import net.javac.utils.EMessageBuilder;
+import net.javac.utils.WelcomeMessageSender;
 import org.jetbrains.annotations.NotNull;
 
 public class Listener extends ListenerAdapter {
     final LogManager logManager;
     final GuildMessageBuffer guildMessageBuffer;
+    final WelcomeMessageSender welcomeMessageSender = new WelcomeMessageSender();
 
     public Listener(GuildMessageBuffer guildMessageBuffer, LogManager logManager) {
         this.logManager = logManager;
@@ -31,6 +34,10 @@ public class Listener extends ListenerAdapter {
 
         if (event instanceof MessageUpdateEvent messageUpdateEvent) {
             logManager.getLogger(MessageUpdateEvent.class).ifPresent(logger -> logger.log(messageUpdateEvent));
+        }
+
+        if (event instanceof GuildMemberJoinEvent guildMemberJoinEvent) {
+            welcomeMessageSender.send(guildMemberJoinEvent);
         }
     }
 }
