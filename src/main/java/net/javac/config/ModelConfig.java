b2be5823ba469a90ce270ac12e24e7f6f46aa103
gokhoso <klasalperen334@gmail.com>
2025-12-22 20:58:01 +0300

    Synchronized to yaml file.

--- src/main/java/net/javac/config/ModelConfig.java
+++ src/main/java/net/javac/config/ModelConfig.java
@@ -1,9 +1,12 @@
 package net.javac.config;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 public class ModelConfig {
     public Bot bot;
     public Config config;
+    public WelcomeMessage welcomeMessage;
     public Channels channels;
     public Roles roles;
 
@@ -17,10 +20,30 @@ public class ModelConfig {
         public int service_pool;
     }
 
+    public static class WelcomeMessage {
+        public String title;
+        public String description;
+        public Fields fields;
+
+        public static class Fields {
+            public int enabled;
+            public int number_of_fields;
+            public List<String> entries;
+            public int suggested_channels;
+            public String suggested_channels_title;
+            public List<String> suggested_channel_list;
+        }
+        public String footer;
+        public String banner;
+    }
+
+
     public static class Channels {
         public String general;
         public String log;
         public String count;
+        public String about;
+        public String rules;
     }
 
     public static class Roles {
