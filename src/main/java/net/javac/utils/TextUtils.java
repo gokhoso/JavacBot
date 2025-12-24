c2068d26747d21d51049f44542d3b497b85f69cb
gokhoso <klasalperen334@gmail.com>
2025-12-24 14:18:07 +0300

    Added setCountVariable method.

--- src/main/java/net/javac/utils/TextUtils.java
+++ src/main/java/net/javac/utils/TextUtils.java
@@ -8,7 +8,13 @@ public class TextUtils {
     public static String createUserMention(String userId) {
         return String.format("<@%s>", userId);
     }
-    public static String setVariable(String text, String guildId, String memberId, String memberName) {
+    public static String setCountVariable(String text, String guildId) {
+        if (text.contains("{count}")) {
+            return text.replace("{count}", Integer.toString(GuildUtils.getCount(guildId)));
+        }
+        return text;
+    }
+    public static String setAllVariables(String text, String guildId, String memberId, String memberName) {
         if (text.contains("{count}")) {
             return text.replace("{count}", Integer.toString(GuildUtils.getCount(guildId)));
         }
