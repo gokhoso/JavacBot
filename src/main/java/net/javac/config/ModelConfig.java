7495c860391ab3a3d43701889ecb51f3a18a813c
gokhoso <klasalperen334@gmail.com>
2025-12-24 14:17:02 +0300

    Added guild.

--- src/main/java/net/javac/config/ModelConfig.java
+++ src/main/java/net/javac/config/ModelConfig.java
@@ -6,13 +6,12 @@ import java.util.List;
 public class ModelConfig {
     public Bot bot;
     public Config config;
-    public WelcomeMessage welcomeMessage;
+    public Guild guild;
     public Channels channels;
     public Roles roles;
 
     public static class Bot {
         public String prefix;
-        public String guild_id;
     }
 
     public static class Config {
@@ -20,23 +19,28 @@ public class ModelConfig {
         public int service_pool;
     }
 
-    public static class WelcomeMessage {
-        public String title;
-        public String description;
-        public Fields fields;
+    public static class Guild {
+        public String guild_id;
+        public String member_count_channel_name;
+        public WelcomeMessage welcomeMessage;
 
-        public static class Fields {
-            public int enabled;
-            public int number_of_fields;
-            public List<String> entries;
-            public int suggested_channels;
-            public String suggested_channels_title;
-            public List<String> suggested_channel_list;
+        public static class WelcomeMessage {
+            public String title;
+            public String description;
+            public Fields fields;
+            public String footer;
+            public String banner;
+
+            public static class Fields {
+                public int enabled;
+                public int number_of_fields;
+                public List<String> entries;
+                public int suggested_channels;
+                public String suggested_channels_title;
+                public List<String> suggested_channel_list;
+            }
         }
-        public String footer;
-        public String banner;
     }
-
 
     public static class Channels {
         public String general;
