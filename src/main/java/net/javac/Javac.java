7e30b65bff20833097f934207cde005bc93c4486
gokhoso <klasalperen334@gmail.com>
2025-12-24 14:16:46 +0300

    Added service manager.

--- src/main/java/net/javac/Javac.java
+++ src/main/java/net/javac/Javac.java
@@ -7,16 +7,22 @@ import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.javac.buffer.impl.GuildMessageBuffer;
 import net.javac.config.ConfigLoader;
+import net.javac.config.ModelConfig;
 import net.javac.log.LogManager;
 import net.javac.log.impl.DeletedMessageLogger;
 import net.javac.log.impl.UpdatedMessageLogger;
+import net.javac.service.ServiceManager;
+import net.javac.service.impl.Count;
+import net.javac.utils.GuildUtils;
 
 import java.util.Arrays;
 
 public class Javac {
     static ShardManager shardManager;
     static final Dotenv dotenv = Dotenv.configure().load();
+    final ModelConfig data = ConfigLoader.getData();
     final LogManager logManager = new LogManager();
+    final ServiceManager serviceManager = new ServiceManager(data.config.service_pool);
 
     static void main() {
         final var token = dotenv.get("TOKEN");
@@ -39,11 +45,16 @@ public class Javac {
         // Wait until login is complete
         shardManager.getShards().getFirst().getSelfUser().getJDA().awaitReady();
 
-        var guildMessageBuffer = new GuildMessageBuffer(ConfigLoader.getData().config.buffer_size);
+        // Set buffer
+        var guildMessageBuffer = new GuildMessageBuffer(data.config.buffer_size);
 
         // Register Logs
         logManager.registerLog(new DeletedMessageLogger(guildMessageBuffer));
         logManager.registerLog(new UpdatedMessageLogger(guildMessageBuffer));
+
+        // Set up and start the services
+        serviceManager.addService("count", new Count(GuildUtils.getGuild(data.guild.guild_id)));
+        serviceManager.start("count");
 
         // Add Listener
         shardManager.addEventListener(new Listener(guildMessageBuffer, logManager));
