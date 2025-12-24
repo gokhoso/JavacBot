5c6f3639427bbcb5ff1df2e86e334ecf8406099e
gokhoso <klasalperen334@gmail.com>
2025-12-24 14:15:56 +0300

    Added getGuild method.

--- src/main/java/net/javac/utils/GuildUtils.java
+++ src/main/java/net/javac/utils/GuildUtils.java
@@ -1,11 +1,16 @@
 package net.javac.utils;
 
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.javac.Javac;
 
 public class GuildUtils {
+    public static Guild getGuild(String id) {
+        return Javac.getShardManager().getGuildById(id);
+    }
+
     public static int getCount(String guildId) {
-        var guild = Javac.getShardManager().getGuildById(guildId);
+        var guild = getGuild(guildId);
         if (guild == null) return 0;
         return guild.getMemberCount();
     }
