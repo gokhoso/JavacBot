e1fab56d84b53b17f205b80ba198f040a07b855b
gokhoso <klasalperen334@gmail.com>
2026-01-02 07:42:16 -0500

    Improved TextUtils.

--- src/main/java/net/javac/utils/TextUtils.java
+++ src/main/java/net/javac/utils/TextUtils.java
@@ -5,25 +5,22 @@ public class TextUtils {
     public static String createChannelMention(String channelId) {
         return String.format("<#%s>", channelId);
     }
+
     public static String createUserMention(String userId) {
         return String.format("<@%s>", userId);
     }
-    public static String setCountVariable(String text, String guildId) {
-        if (text.contains("{count}")) {
-            return text.replace("{count}", Integer.toString(GuildUtils.getCount(guildId)));
+
+    public static String countOrdinalSuffix(int count) {
+        final int mod100 = count % 100;
+        if (mod100 >= 11 && mod100 <= 13) {
+            return count + "th";
         }
-        return text;
-    }
-    public static String setAllVariables(String text, String guildId, String memberId, String memberName) {
-        if (text.contains("{count}")) {
-            return text.replace("{count}", Integer.toString(GuildUtils.getCount(guildId)));
-        }
-        if (text.contains("{member.name}")) {
-            return text.replace("{member.name}", memberName);
-        }
-        if (text.contains("{member.mention}")) {
-            return text.replace("{member.mention}", TextUtils.createUserMention(memberId));
-        }
-        return text;
+        final int mod10 = count % 10;
+        return switch (mod10) {
+            case 1 -> count + "st";
+            case 2 -> count + "nd";
+            case 3 -> count + "rd";
+            default -> count + "th";
+        };
     }
 }
