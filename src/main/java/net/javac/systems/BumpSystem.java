c330893e53818a105397b2f5ec7741a09f738efe
gokhoso <klasalperen334@gmail.com>
2026-01-02 07:40:09 -0500

    Updated for imports and changed signature.

--- src/main/java/net/javac/systems/BumpSystem.java
+++ src/main/java/net/javac/systems/BumpSystem.java
@@ -5,85 +5,76 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.javac.config.ConfigLoader;
 import net.javac.utils.GuildUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.javac.utils.TextVariables;
 
 import java.awt.*;
+import java.util.Objects;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static net.javac.utils.TextUtils.setAllVariables;
-
 public class BumpSystem {
-    final Logger logger = LoggerFactory.getLogger(BumpSystem.class);
     private final ScheduledExecutorService scheduledExecutorService;
+    private final TextVariables textVariables = new TextVariables();
 
     public BumpSystem(ScheduledExecutorService scheduledExecutorService) {
         this.scheduledExecutorService = scheduledExecutorService;
     }
 
-    MessageEmbed onBumpEmbed(String guildId, String memberName, String memberId, String guildPhotoUrl) {
-        var ob = ConfigLoader.getData().systems.bump.on_bump;
+    MessageEmbed onBumpEmbed(String name, String title, String description, Color color, String iconUrl, String footer) {
         var embed = new EmbedBuilder();
-        // Set embed values
-        embed.setAuthor(memberName, null, guildPhotoUrl);
-        embed.setColor(Color.magenta);
-        embed.setTitle(setAllVariables(ob.title, guildId, memberId, memberName));
-        embed.setDescription(setAllVariables(ob.description, guildId, memberId, memberName));
-        embed.setFooter(ob.footer);
-        if (ob.guild_thumbnail == 1) {
-            embed.setThumbnail(guildPhotoUrl);
-        }
+        embed.setTitle(title);
+        embed.setAuthor(name, null, null);
+        embed.setDescription(description);
+        embed.setColor(color);
+        embed.setFooter(footer);
+        embed.setThumbnail(iconUrl);
         return embed.build();
     }
 
-    MessageEmbed bumpReminderEmbed(String guildId, String memberName, String memberId, String iconUrl) {
-        var rb = ConfigLoader.getData().systems.bump.reminder_bump;
+    MessageEmbed bumpReminderEmbed(String name, String title, String desc, Color color, String iconUrl, String footer) {
         var embed = new EmbedBuilder();
-        // Set embed values
-        embed.setAuthor(memberName, null, iconUrl);
-        embed.setColor(Color.magenta);
-        embed.setTitle(setAllVariables(rb.title, guildId, memberId, memberName));
-        embed.setDescription(setAllVariables(rb.description, guildId, memberId, memberName));
-        embed.setFooter(rb.footer);
-        if (rb.guild_thumbnail == 1) {
-            embed.setThumbnail(iconUrl);
-        }
+        embed.setTitle(title);
+        embed.setAuthor(name, null, null);
+        embed.setDescription(desc);
+        embed.setColor(color);
+        embed.setFooter(footer);
+        embed.setThumbnail(iconUrl);
         return embed.build();
     }
 
-    public void send(MessageReceivedEvent e) {
-        if (!e.getMessageId().equals("302050872383242240")) {
-            return;
-        }
-        if (e.getMessage().getEmbeds().size() != 1) {
-            return;
-        }
-        var desc = e.getMessage().getEmbeds().getFirst().getDescription();
-        if (desc != null && !desc.contains("Öne çıkarma başarılı!")) {
-            return;
-        }
-        final var guild = e.getGuild();
+    public void send(MessageReceivedEvent event) {
+        if (event.getMessage().getEmbeds().size() != 1) return;
+        final String desc = event.getMessage().getEmbeds().getFirst().getDescription();
+        if (desc == null || !desc.contains("Öne çıkarma başarılı!")) return;
+
+        final var guild = event.getGuild();
         final String guildId = guild.getId();
-        final var member = e.getMember();
-        if (member == null) {
-            logger.warn("Member is null can not send Bump message");
-            return;
-        }
-        final String memberId = member.getId();
-        final String memberName = member.getUser().getName();
-        final String iconUrl = guild.getIconUrl();
-        final var embed = onBumpEmbed(guildId, memberName, memberId, iconUrl);
-        if (embed == null) {
-            logger.error("Embed is null, send failed.");
-            return;
-        }
-        final var channel = GuildUtils.getTextChannel(guildId, ConfigLoader.getData().guild.channels.general);
-        if (channel == null) {
-            logger.error("General channel is null [send method]");
-            return;
-        }
-        e.getMessage().getChannel().asTextChannel().sendMessageEmbeds(embed).queue();
-        scheduledExecutorService.schedule(() -> channel.sendMessageEmbeds(bumpReminderEmbed(guildId, memberName, memberId, iconUrl)).queue(), 2, TimeUnit.HOURS);
+        final var member = event.getMember();
+        if (member == null) return;
+
+        final String memberId = Objects.requireNonNull(event.getMessage().getInteractionMetadata()).getUser().getId();
+        final String name = Objects.requireNonNull(event.getMessage().getInteractionMetadata()).getUser().getName();
+
+        textVariables.member(memberId, name);
+
+        var ob = ConfigLoader.getData().systems.bump.on_bump;
+        var ob_title = textVariables.apply(ob.title);
+        var ob_desc = textVariables.apply(ob.description);
+        var ob_footer = textVariables.apply(ob.footer);
+        var ob_color = Color.getColor(ob.color);
+        final String ob_iconUrl = ob.guild_thumbnail == 1 ? guild.getIconUrl() : null;
+        final var ob_embed = onBumpEmbed(name, ob_title, ob_desc, ob_color, ob_iconUrl, ob_footer);
+
+        event.getMessage().getChannel().asTextChannel().sendMessageEmbeds(ob_embed).queue();
+
+        var rb = ConfigLoader.getData().systems.bump.reminder_bump;
+        var rb_title = textVariables.apply(rb.title);
+        var rb_desc = textVariables.apply(rb.description);
+        var rb_footer = textVariables.apply(rb.footer);
+        var rb_color = Color.getColor(rb.color);
+        var rb_thumbnail = rb.guild_thumbnail == 1 ? guild.getIconUrl() : null;
+
+        final var bumpedChannel = Objects.requireNonNull(GuildUtils.getTextChannel(guildId, ConfigLoader.getData().guild.channels.general));
+        scheduledExecutorService.schedule(() -> bumpedChannel.sendMessageEmbeds(bumpReminderEmbed(name, rb_title, rb_desc, rb_color, rb_thumbnail, rb_footer)).queue(), 2, TimeUnit.HOURS);
     }
 }
