ae9f0d00c9bf60dd2b5d36a5a3747aa759f51dbd
gokhoso <klasalperen334@gmail.com>
2026-01-05 11:45:20 -0500

    Added permissions.

--- src/main/java/net/javac/command/CommandInformation.java
+++ src/main/java/net/javac/command/CommandInformation.java
@@ -1,3 +1,5 @@
 package net.javac.command;
 
-public record CommandInformation(String name, String description, String usage) {}
+import net.dv8tion.jda.api.Permission;
+
+public record CommandInformation(String name, String description, Permission permission, Boolean isOwnerCommand, String usage) {}
