0e1bea0cd3a88db23d6bcfb7c5286430cfcfc144
gokhoso <klasalperen334@gmail.com>
2026-01-05 11:48:43 -0500

    Added context.

--- src/main/java/net/javac/command/ICommand.java
+++ src/main/java/net/javac/command/ICommand.java
@@ -1,9 +1,7 @@
 package net.javac.command;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-
 @SuppressWarnings("unused")
 public interface ICommand {
     CommandInformation getInformation();
-    void execute(MessageReceivedEvent event);
+    void execute(CommandContext ctx);
 }
