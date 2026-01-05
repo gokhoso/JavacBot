0be95207515ec696ef3bc9f5fe8ed17258216fd6
gokhoso <klasalperen334@gmail.com>
2026-01-05 11:43:23 -0500

    Minor fixes.

--- src/main/java/net/javac/systems/BumpSystem.java
+++ src/main/java/net/javac/systems/BumpSystem.java
@@ -20,26 +20,27 @@ public class BumpSystem {
         this.scheduledExecutorService = scheduledExecutorService;
     }
 
-    MessageEmbed onBumpEmbed(String name, String title, String description, Color color, String iconUrl, String footer) {
-        var embed = new EmbedBuilder();
-        embed.setTitle(title);
-        embed.setAuthor(name, null, null);
-        embed.setDescription(description);
-        embed.setColor(color);
-        embed.setFooter(footer);
-        embed.setThumbnail(iconUrl);
-        return embed.build();
+    MessageEmbed onBumpEmbed(String name, String avatarUrl, String image, String title, String description, Color color, String iconUrl, String footer) {
+        return new EmbedBuilder()
+        .setTitle(title)
+        .setAuthor(name, null, avatarUrl)
+        .setDescription(description)
+        .setColor(color)
+        .setImage(image)
+        .setFooter(footer)
+        .setThumbnail(iconUrl)
+        .build();
     }
 
-    MessageEmbed bumpReminderEmbed(String name, String title, String desc, Color color, String iconUrl, String footer) {
-        var embed = new EmbedBuilder();
-        embed.setTitle(title);
-        embed.setAuthor(name, null, null);
-        embed.setDescription(desc);
-        embed.setColor(color);
-        embed.setFooter(footer);
-        embed.setThumbnail(iconUrl);
-        return embed.build();
+    MessageEmbed bumpReminderEmbed(String title, String desc, Color color, String iconUrl, String image, String footer) {
+        return new EmbedBuilder()
+        .setTitle(title)
+        .setDescription(desc)
+        .setColor(color)
+        .setImage(image)
+        .setFooter(footer)
+        .setThumbnail(iconUrl)
+        .build();
     }
 
     public void send(MessageReceivedEvent event) {
@@ -52,18 +53,21 @@ public class BumpSystem {
         final var member = event.getMember();
         if (member == null) return;
 
-        final String memberId = Objects.requireNonNull(event.getMessage().getInteractionMetadata()).getUser().getId();
-        final String name = Objects.requireNonNull(event.getMessage().getInteractionMetadata()).getUser().getName();
+        final var interactionMember = Objects.requireNonNull(event.getMessage().getInteractionMetadata()).getUser();
+        final String userId = interactionMember.getId();
+        final String userName = interactionMember.getName();
+        final String userAvatarUrl = interactionMember.getEffectiveAvatarUrl();
 
-        textVariables.member(memberId, name);
+        textVariables.member(userId, userName);
 
         var ob = ConfigLoader.getData().systems.bump.on_bump;
         var ob_title = textVariables.apply(ob.title);
         var ob_desc = textVariables.apply(ob.description);
         var ob_footer = textVariables.apply(ob.footer);
         var ob_color = Color.getColor(ob.color);
+        var ob_img = ob.image;
         final String ob_iconUrl = ob.guild_thumbnail == 1 ? guild.getIconUrl() : null;
-        final var ob_embed = onBumpEmbed(name, ob_title, ob_desc, ob_color, ob_iconUrl, ob_footer);
+        final var ob_embed = onBumpEmbed(userName, userAvatarUrl, ob_img, ob_title, ob_desc, ob_color, ob_iconUrl, ob_footer);
 
         event.getMessage().getChannel().asTextChannel().sendMessageEmbeds(ob_embed).queue();
 
@@ -72,9 +76,10 @@ public class BumpSystem {
         var rb_desc = textVariables.apply(rb.description);
         var rb_footer = textVariables.apply(rb.footer);
         var rb_color = Color.getColor(rb.color);
+        var rb_img = rb.image;
         var rb_thumbnail = rb.guild_thumbnail == 1 ? guild.getIconUrl() : null;
 
         final var bumpedChannel = Objects.requireNonNull(GuildUtils.getTextChannel(guildId, ConfigLoader.getData().guild.channels.general));
-        scheduledExecutorService.schedule(() -> bumpedChannel.sendMessageEmbeds(bumpReminderEmbed(name, rb_title, rb_desc, rb_color, rb_thumbnail, rb_footer)).queue(), 2, TimeUnit.HOURS);
+        scheduledExecutorService.schedule(() -> bumpedChannel.sendMessageEmbeds(bumpReminderEmbed(rb_title, rb_desc, rb_color, rb_thumbnail, rb_img, rb_footer)).queue(), 2, TimeUnit.HOURS);
     }
 }
